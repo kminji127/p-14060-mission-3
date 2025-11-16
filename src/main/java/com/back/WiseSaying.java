6446aadeb92bd982f64065711ebe00f458a383dd
Minji Kim <sml07113@gmail.com>
2025-11-17 02:15:48 +0900

    refactor: getter/setter 추가

--- src/main/java/com/back/WiseSaying.java
+++ src/main/java/com/back/WiseSaying.java
@@ -1,31 +1,14 @@
 package com.back;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @AllArgsConstructor // 모든 필드 생성자
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
-
-    public int getId() {
-        // 같은 이름의 지역 변수나 매개변수가 없으면 this 생략 가능
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
