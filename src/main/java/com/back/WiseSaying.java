720395c450a0844671a2e48253a29f550a34aed6
Minji Kim <sml07113@gmail.com>
2025-11-17 02:26:36 +0900

    refactor: 필수 생성자인 id에 final 추가 및 RequiredArgsConstructor 추가

--- src/main/java/com/back/WiseSaying.java
+++ src/main/java/com/back/WiseSaying.java
@@ -2,13 +2,16 @@ package com.back;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor // 모든 필드 생성자
+@AllArgsConstructor  // 모든 필드 생성자
+//@NoArgsConstructor  // 인자 없는 생성자 (final 불가)
+@RequiredArgsConstructor // 필수 생성자 자동 생성 (final, @NotNull 필드)
 public class WiseSaying {
-    private int id;
+    private final int id; // 변경 불가하므로 final 추가 -> 생성자에서 반드시 초기화해야 함
     private String content;
     private String author;
 }
